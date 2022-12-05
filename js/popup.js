f389e9d66102207519e1f69ba94679a93b1bbff7
Tetiana Chykalova <chikalova.t@gmail.com>
2022-12-05 11:33:41 +0100

    added validation and tel

--- js/popup.js
+++ js/popup.js
@@ -11,3 +11,44 @@ popupClose.addEventListener('click', function () {
     popup.classList.remove('open')
     document.body.classList.remove('overflow')
 })
+
+
+const phoneDiv = document.querySelector('.popup__form-tel')
+const phoneInputField = document.querySelector("#popup-tel");
+const phoneInput = window.intlTelInput(phoneInputField, {
+    preferredCountries: ["ua", "pl"],
+    utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+
+
+const requestInput = document.querySelectorAll('.req')
+const submitBtn = document.querySelector('.popup__btn-sale')
+
+function addErr(input) {
+    input.classList.add('error')
+}
+
+submitBtn.addEventListener('click', function () {
+    for (let i = 0; i < requestInput.length; i++) {
+        let input = requestInput[i].querySelector('input')
+        if(!input.value) {
+            addErr(requestInput[i])
+            submitBtn.setAttribute('disabled', false)
+        }
+    }
+})
+
+function changedInput() {
+    for (let i = 0; i < requestInput.length; i++) {
+        let input = requestInput[i].querySelector('input')
+        input.addEventListener('input', function () {
+            requestInput[i].classList.remove('error')
+            submitBtn.removeAttribute('disabled')
+        })
+    }
+}
+
+changedInput()
+
+
